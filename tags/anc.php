1a2d33b6f6b20c88c0532431a07c3d23cb2df2e1
Veisakis <74550377+Veisakis@users.noreply.github.com>
2021-09-23 10:30:02 +0300

    Update anc.php

--- tags/anc.php
+++ tags/anc.php
@@ -6,40 +6,38 @@
     <link rel="stylesheet" href="../css/style.css">
   </head>
   <body>
-		<section>
-			<img src="../img/tags/anc.jpeg" alt="Μέγας Αλέξανδρος" class="tag-image">
-		</section>
-		<hr id='taghr'>
-		<div class="titles">
+    <section>
+      <img src="../img/tags/anc.jpeg" alt="Μέγας Αλέξανδρος" class="tag-image">
+    </section>
+    <hr id="taghr">
+    <div class="titles">
     <?php
-	
     $articles_name = array();
     $articles_value = array();
-		$output=null;
-		$retval=null;
-
-		exec("grep tags ../src/* | grep 'Αρχαία Ελλάδα'| cut -f1 | sed s/.$// | sed s/^.*src// | sed s/^.//", $output, $retval);
-		foreach ($output as $out){
-    	$striped_file = str_replace(".html", "", $out);
+    $output=null;
+    $retval=null;
+    exec("grep tags ../src/* | grep 'Αρχαία Ελλάδα'| cut -f1 | sed s/.$// | sed s/^.*src// | sed s/^.//", $output, $retval);
+	    
+    foreach ($output as $out){
+        $striped_file = str_replace(".html", "", $out);
     	$articles_name[] = $striped_file;
-        
-	  	$divided = explode(' - ', $striped_file);
+	$divided = explode(' - ', $striped_file);
         
     	if (strpos($divided[0],'πΧ') == true){
-      	$date_code = explode(' ',$divided[0]);  
-      	$articles_value[] = number_format('-' . $date_code[0]);
-      }
-      else{
-        $articles_value[] = number_format($divided[0]);
-      }
-		}
+      	  $date_code = explode(' ',$divided[0]);  
+      	  $articles_value[] = number_format('-' . $date_code[0]);
+        }
+        else{
+          $articles_value[] = number_format($divided[0]);
+        }
+    }
     $articles = array_combine($articles_name,$articles_value);	
     asort($articles);
 
-    foreach ($articles as $article => $val) {
+    foreach ($articles as $article => $val){
           echo "<a href='../src/$article.html'>$article</a><br/>";
     }
   ?>
-	</div>
+  </div>
   </body>
 </html>
